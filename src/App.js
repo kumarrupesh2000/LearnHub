20ebb48214855f3f48d29007fc83853ad9cef36f
kumarrupesh2000 <kumarrupesh2000@gmail.com>
2024-03-31 17:51:46 +0530

    About page added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ForgotPassword from "./pages/ForgotPassword";
 import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import MyProfile from "./components/core/Dashboard/MyProfile";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -54,6 +55,14 @@ function App() {
               <VerifyEmail />
           }
         />  
+         <Route
+          path="/about"
+          element={
+            
+              <About />
+            
+          }
+        />
          <Route path="dashboard/my-profile" element={<MyProfile />} />
 
       </Routes>
