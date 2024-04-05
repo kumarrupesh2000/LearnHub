065a205b09a7254b764b5fdcd0b4067700144ad1
kumarrupesh2000 <kumarrupesh2000@gmail.com>
2024-04-05 11:48:34 +0530

    message format updated

--- server/mail/templates/courseEnrollmentEmail.js
+++ server/mail/templates/courseEnrollmentEmail.js
@@ -67,7 +67,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
     
     <body>
         <div class="container">
-            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+            <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.postimg.cc/hP10zgXC/Dark.png"
                     alt="StudyNotion Logo"></a>
             <div class="message">Course Registration Confirmation</div>
             <div class="body">
@@ -79,7 +79,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
                 <a class="cta" href="https://studynotion-edtech-project.vercel.app/dashboard">Go to Dashboard</a>
             </div>
             <div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-                    href="mailto:info@studynotion.com">info@studynotion.com</a>. We are here to help!</div>
+                    href="mailto:kumarrupesh2000@gmail.com">info@learnhub.com</a>. We are here to help!</div>
         </div>
     </body>
     
--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -15,7 +15,7 @@ const mailSender=async(email,title,body)=>{
             }
         });
         const info=await transporter.sendMail({
-            from: 'Sona Jeweller & Company', 
+            from: 'LearnHub||Rupesh', 
             to: `${email}`,
             subject: `${title}`,
             html:`${body}`,
